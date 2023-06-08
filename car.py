5fe6de39cbede959bc4fdcf14641dde1ae724f94
AimenKhalid338 <121729095+Aimen-Khalid@users.noreply.github.com>
2023-06-08 21:13:34 +0500

    car.py refactored

--- car.py
+++ car.py
@@ -18,12 +18,6 @@ params = {
     "P_PARALLEL_ACC_WEIGHT": 0.2,
     "angle_threshold": 5
 }
-# dt = 0.01
-# D_PER_ACC_WEIGHT = 20
-# P_PER_ACC_WEIGHT = 10
-# P_PARALLEL_DEC_WEIGHT = 10
-# P_PARALLEL_ACC_WEIGHT = 0.2
-# angle_threshold = 5
 
 
 class Vehicle:
@@ -44,19 +38,23 @@ class Vehicle:
 
         # state variables
         self.centroid = centroid
+        self.front_mid_point = None
+        self.lookahead_point = None
+        self.closest_point = None
         self.angle = angle
         self.velocity = velocity
         self.perpendicular_acc = acceleration
         self.parallel_acc = geometry.get_vector(0, 0)
+        self.parallel_acc_sign = 1
+        self.perpendicular_acc_sign = 1
         self.lookahead_angle = 0
         self.error = 0
         self.prev_error = 0
         self.current_face = None
-        self.prev_face = None
         self.face_mid_point = None
 
     def state_to_corners(self):
-        """uses centroid and angle of vehicle to return its 4 points"""
+        """Returns car's four points using centroid and vehicle orientation"""
 
         # dir_vector starts from vehicle centroid and ends at the mid-point of the front line of vehicle
         dir_vect = ScaledPoint(cos(self.angle), sin(self.angle))
@@ -78,10 +76,11 @@ class Vehicle:
         proportional_error = self.error
         derivative_error = self.error - self.prev_error
 
-        perp_acc_magnitude = (params["P_PER_ACC_WEIGHT"] * proportional_error) + (params["D_PER_ACC_WEIGHT"] * derivative_error)
+        perpendicular_acc_magnitude = (params["P_PER_ACC_WEIGHT"] * proportional_error) + \
+                                    (params["D_PER_ACC_WEIGHT"] * derivative_error)
+        self.perpendicular_acc_sign = -1 if perpendicular_acc_magnitude < 0 else 1
 
-        self.perpendicular_acc = geometry.get_vector(self.theta + (self.angle * 180 / math.pi),
-                                                               perp_acc_magnitude)
+        self.perpendicular_acc = geometry.get_vector(self.theta + (self.angle * 180 / math.pi), perpendicular_acc_magnitude)
         self.perpendicular_acc = geometry.keep_in_limit(self.perpendicular_acc, self.acc_limit)
 
         if self.velocity.norm() >= self.goal_speed:
@@ -89,11 +88,14 @@ class Vehicle:
 
     def set_parallel_acc(self):
         if abs(self.lookahead_angle) > radians(params["angle_threshold"]):
-            dec_magnitude = self.lookahead_angle - radians(params["angle_threshold"])
-            self.parallel_acc = (self.velocity / self.velocity.norm()) * dec_magnitude * (-params["P_PARALLEL_DEC_WEIGHT"])
+            dec_magnitude = (self.lookahead_angle - radians(params["angle_threshold"])) * (-params["P_PARALLEL_DEC_WEIGHT"])
+            self.parallel_acc_sign = -1 if dec_magnitude * (-params["P_PARALLEL_DEC_WEIGHT"]) < 0 else 1
+            self.parallel_acc = (self.velocity / self.velocity.norm()) * dec_magnitude
         else:
-            self.parallel_acc = (self.velocity / self.velocity.norm()) * (
-                    params["P_PARALLEL_ACC_WEIGHT"] * (self.goal_speed - self.velocity.norm()))
+            acc_magnitude = params["P_PARALLEL_ACC_WEIGHT"] * (self.goal_speed - self.velocity.norm())
+            self.parallel_acc = (self.velocity / self.velocity.norm()) * acc_magnitude
+            self.parallel_acc_sign = -1 if acc_magnitude < 0 else 1
+
         self.parallel_acc = geometry.keep_in_limit(self.parallel_acc, self.acc_limit)
 
     def controller(self):
@@ -102,13 +104,49 @@ class Vehicle:
         resultant_acc = self.perpendicular_acc + self.parallel_acc
         return geometry.keep_in_limit(resultant_acc, self.acc_limit)
 
-    def update_state_vars(self):
-        self.error = self.get_error_ang_disp()
+    def set_velocity(self):
         acc = self.controller()
         self.velocity += (acc * params["dt"])
         self.velocity = geometry.keep_in_limit(self.velocity, self.speed_limit)
+
+    def set_centroid(self):
         self.centroid += self.velocity * params["dt"]
+
+    def set_closest_point(self):
+        projected_point_distance = self.reference_track.project(self.front_mid_point)
+        self.closest_point = self.reference_track.interpolate(projected_point_distance)
+
+    def set_lookahead_point(self):
+        """ Creates a circle with center at car centroid and radius equal to lookahead distance.
+            Finds the intersection points of the circle and track.
+            Returns the intersection point closest to the car's velocity vector
+        """
+        circle_center = Point(self.centroid.get_x(), self.centroid.get_y())
+        circle_radius = self.lookahead_distance
+        vel_vector = Point(self.velocity.get_x(), self.velocity.get_y())
+        circle = circle_center.buffer(circle_radius)  # Create a circle geometry
+        boundary = circle.boundary  # Get the boundary of the circle
+        intersection = boundary.intersection(self.reference_track)
+        if intersection.is_empty:
+            return None
+        # Translate the velocity vector to car centroid
+        vel_vector = Point(vel_vector.x + circle_center.x, vel_vector.y + circle_center.y)
+        # If multiple intersection points exist, find the one closest to the velocity vector
+        if intersection.geom_type == 'MultiPoint':
+            intersection = nearest_points(vel_vector, intersection)[1]
+        self.lookahead_point = intersection
+
+    def set_orientation(self):
         self.angle = math.atan2(self.velocity.get_y(), self.velocity.get_x())
+
+    def update_state_vars(self):
+        self.set_vehicle_front_mid_point()
+        self.set_closest_point()
+        self.set_lookahead_point()
+        self.set_error()
+        self.set_velocity()
+        self.set_centroid()
+        self.set_orientation()
 
     def get_xy_lists(self):
         p = self.state_to_corners()
@@ -124,124 +162,44 @@ class Vehicle:
 
         return [(x1, y1), (x2, y2)]
 
-    def get_vehicle_front_mid_point(self):
+    def set_vehicle_front_mid_point(self):
         """
         :return: A Point object representing the mid-point of the front line of the vehicle.
         """
         segment = self.get_car_perpendicular_line()
-        return Point((segment[0][0] + segment[1][0]) / 2, (segment[0][1] + segment[1][1]) / 2)
+        self.front_mid_point = Point((segment[0][0] + segment[1][0]) / 2, (segment[0][1] + segment[1][1]) / 2)
 
-    def get_projected_points(self):
-        vehicle_point = self.get_vehicle_front_mid_point()
-        projected_point_distance = self.reference_track.project(vehicle_point)
-        closest_point = self.reference_track.interpolate(projected_point_distance)
-        lookahead_point = self.reference_track.interpolate(projected_point_distance + self.lookahead_distance)
-        # return {"closest_point": closest_point, "lookahead_point": lookahead_point}
-        return {"closest_point": closest_point, "lookahead_point": self.get_lookahead_point()}
+    def get_lookahead_angle_magnitude(self):
+        """Returns lookahead angle magnitude in radians"""
+        headed_direction = (self.front_mid_point, Point(self.velocity.x + self.front_mid_point.x, self.velocity.y + self.front_mid_point.y))
+        track_direction = (self.front_mid_point, self.lookahead_point)
 
-    def get_lookahead_point(self):
-        circle_center = Point(self.centroid.get_x(), self.centroid.get_y())
-        circle_radius = self.lookahead_distance
-        vel_vector = Point(self.velocity.get_x(), self.velocity.get_y())
-        circle = Point(circle_center).buffer(circle_radius)  # Create a circle geometry
-        boundary = circle.boundary  # Get the boundary of the circle
-        intersection = boundary.intersection(self.reference_track)  # Find the intersection points
-        if intersection.is_empty:
-            return None  # No intersection point
-        vel_vector = Point(vel_vector.x + circle_center.x, vel_vector.y + circle_center.y)
-        # If multiple intersection points exist, find the one closest to the circle center
-        if intersection.geom_type == 'MultiPoint':
-            intersection = nearest_points(vel_vector, intersection)[1]
+        vector1 = Point(headed_direction[1].x - headed_direction[0].x, headed_direction[1].y - headed_direction[0].y)
+        vector2 = Point(track_direction[1].x - track_direction[0].x, track_direction[1].y - track_direction[0].y)
 
-        return intersection
+        dot_product = vector1.x * vector2.x + vector1.y * vector2.y
 
-    def get_track_segment(self):
-        """
-        :return: A tuple of two Point objects representing the start and end points of the track segment
-                 on which the look-ahead point lies
-        """
-        lookahead_point = self.get_projected_points()["lookahead_point"]
+        magnitude1 = math.sqrt(vector1.x ** 2 + vector1.y ** 2)
+        magnitude2 = math.sqrt(vector2.x ** 2 + vector2.y ** 2)
 
-        # Iterate over all the line segments of the reference track and find the segment closest to the projected point
-        coordinates = self.reference_track.coords
-        for i in range(len(coordinates) - 1):
-            line_segment = LineString([coordinates[i], coordinates[i + 1]])
-            if line_segment.distance(lookahead_point) < 1:
-                # If the projected point is within 1 unit of distance from the line segment, return the start and end points
-                return Point(coordinates[i][0], coordinates[i][1]), Point(coordinates[i + 1][0], coordinates[i + 1][1])
+        return math.acos(dot_product / (magnitude1 * magnitude2))
 
-    def get_error_from_track(self):
-        """
-        :return: The signed error, positive if the vehicle is to the right of the track and negative if it is to the left.
-        """
-        vehicle_point = self.get_vehicle_front_mid_point()
-        segment_point_1, segment_point_2 = self.get_track_segment()
-
-        error = vehicle_point.distance(self.reference_track)
+    def get_error_sign(self):
+        segment_point_1 = Point(self.front_mid_point.x, self.front_mid_point.y)
+        segment_point_2 = Point(self.velocity.get_x() + self.front_mid_point.x, self.velocity.get_y() + self.front_mid_point.y)
 
         # translating the points to origin
-        vehicle_point = Point(vehicle_point.x - segment_point_1.x, vehicle_point.y - segment_point_1.y)
-        segment_point_2 = Point(segment_point_2.x - segment_point_1.x, segment_point_2.y - segment_point_1.y)
-
-        cross_product = segment_point_2.x * vehicle_point.y - segment_point_2.y * vehicle_point.x
-
-        return error * (-1 if cross_product < 0 else 1)
-
-    def get_error_ang_disp(self):
-        vehicle_point = (self.get_vehicle_front_mid_point().x, self.get_vehicle_front_mid_point().y)
-        headed_direction = (
-            vehicle_point, (self.velocity.get_x() + vehicle_point[0], self.velocity.get_y() + vehicle_point[1]))
-        # print(headed_direction)
-        lookahead_point = (self.get_projected_points()["lookahead_point"].x,
-                           self.get_projected_points()["lookahead_point"].y)
-        track_direction = (vehicle_point, lookahead_point)
-        # print(track_direction)
-        # Calculate the vectors of the two line segments
-        vector1 = (headed_direction[1][0] - headed_direction[0][0], headed_direction[1][1] - headed_direction[0][1])
-        vector2 = (track_direction[1][0] - track_direction[0][0], track_direction[1][1] - track_direction[0][1])
-
-        # Calculate the dot product of the two vectors
-        dot_product = vector1[0] * vector2[0] + vector1[1] * vector2[1]
-
-        # Calculate the magnitudes of the vectors
-        magnitude1 = math.sqrt(vector1[0] ** 2 + vector1[1] ** 2)
-        magnitude2 = math.sqrt(vector2[0] ** 2 + vector2[1] ** 2)
-
-        # Calculate the angle in radians
-        self.lookahead_angle = math.acos(dot_product / (magnitude1 * magnitude2))
-
-        lookahead_point = Point(lookahead_point[0], lookahead_point[1])
-        segment_point_1, segment_point_2 = Point(vehicle_point[0], vehicle_point[1]), \
-            Point(self.velocity.get_x() + vehicle_point[0], self.velocity.get_y() + vehicle_point[1])
-
-        # translating the points to origin
-        lookahead_point = Point(lookahead_point.x - segment_point_1.x, lookahead_point.y - segment_point_1.y)
+        lookahead_point = Point(self.lookahead_point.x - segment_point_1.x, self.lookahead_point.y - segment_point_1.y)
         segment_point_2 = Point(segment_point_2.x - segment_point_1.x, segment_point_2.y - segment_point_1.y)
 
         cross_product = segment_point_2.x * lookahead_point.y - segment_point_2.y * lookahead_point.x
 
-        return self.lookahead_angle * (-1 if cross_product > 0 else 1)
+        return -1 if cross_product > 0 else 1
 
-    def get_vectors(self):
-        vehicle_point = (self.get_vehicle_front_mid_point().x, self.get_vehicle_front_mid_point().y)
-        headed_direction = (
-            vehicle_point, (self.velocity.get_x() + vehicle_point[0], self.velocity.get_y() + vehicle_point[1]))
-        lookahead_point = (
-            self.get_projected_points()["lookahead_point"].x, self.get_projected_points()["lookahead_point"].y)
-        track_direction = (vehicle_point, lookahead_point)
-        return headed_direction, track_direction
+    def set_error(self):
+        self.lookahead_angle = self.get_lookahead_angle_magnitude()
+        self.error = self.lookahead_angle * self.get_error_sign()
 
     def set_current_face(self, road_network):
-        """
-        A utility function for compute_parameters function
-
-        :param road_network: The road network DCEL object
-        :param self: The vehicle whose current face needs to be set.
-        """
-        vehicle_x, vehicle_y = self.get_vehicle_front_mid_point().x, self.get_vehicle_front_mid_point().y
+        vehicle_x, vehicle_y = self.front_mid_point.x, self.front_mid_point.y
         self.current_face = road_network.get_face_for_point((vehicle_x, vehicle_y))
-
-        if self.current_face is None:
-            self.current_face = self.prev_face
-        if self.current_face is not None:
-            self.prev_face = self.current_face
